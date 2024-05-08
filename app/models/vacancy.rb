43db66d2b50842e6d1f1ae979a773546c9d900ee
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-05-07 22:38:25 -0300

    Adding restrictions to vacancy creation

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,4 +1,17 @@
 class Vacancy < ApplicationRecord
+  validate :one_per_user
+  validate :under_talk_limit
+
   belongs_to :user
   belongs_to :talk
+
+  def one_per_user
+    vacancies_by_same_user = Vacancy.all.find(user_id: user.id)
+    vacancies_by_same_user.empty?
+  end
+
+  def under_talk_limit
+    vacancies_by_talk = Vacancy.all.find(talk_id: talk.id)
+    vacancies_by_talk.length < talk.vacancy_limit
+  end
 end
--- spec/requests/talks_spec.rb
+++ spec/requests/talks_spec.rb
@@ -42,6 +42,7 @@ RSpec.describe '/talks', type: :request do
             description: 'Um exemplo de palestra',
             start_date: '2024-04-27 12:00',
             end_date: '2024-04-27 14:00',
+            vacancy_limit: 50,
             event_id: event.id,
             location_id: location.id,
           }
