b2f2a63132d90f61546f89d9571b75aa24ff29a1
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-05-16 13:16:53 -0300

    Adding some missing validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :name, uniqueness: true
+
   has_many :merches
   has_many :talks
   belongs_to :team, optional: true
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
+  validate :unique_per_user
+
   belongs_to :user
   belongs_to :merch
+
+  def unique_per_user
+    conflicting_reservations = Reservation.where('user_id = :user_id AND merch_id = :merch_id AND id != :id',
+                                                 { user_id: user.id, merch_id: merch.id, id: id })
+    errors.add(:already_reserved, 'You already have a reservation for this merch!') unless conflicting_reservations.empty?
+  end
 end
