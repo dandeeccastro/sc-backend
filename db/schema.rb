6ad263d6e8d4d8be7da4f9da728699a12e71ea54
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-05-26 21:26:09 -0300

    Final slug on events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_09_200112) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_24_134203) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_09_200112) do
     t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["team_id"], name: "index_events_on_team_id"
   end
 
