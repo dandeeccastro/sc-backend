47d4733d10fb4b9da6fb02c04dd17d574eead4b8
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-08 12:44:55 -0300

    validating event and talk dates on creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validate :invalidates_talks
+
   before_save :set_slug
 
   validates :name, uniqueness: true
@@ -7,6 +9,11 @@ class Event < ApplicationRecord
   has_many :merches
   has_many :talks
   has_one :team
+
+  def invalidates_talks
+    talks = Talk.where('start_date < :start_date OR end_date > :end_date', { start_date: start_date, end_date: end_date})
+    errors.add(:invalidated_talks, "Existem #{talks.count} palestras que conflitam com o horário do evento") unless talks.empty?
+  end
 
   private
 
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,5 +1,6 @@
 class Talk < ApplicationRecord
   validate :overlaps_with_other_talk
+  validate :outside_event_bounds
 
   belongs_to :event
   belongs_to :location
@@ -14,5 +15,10 @@ class Talk < ApplicationRecord
       { start_date: start_date, end_date: end_date, location: location_id, id: id, event: event_id }
     )
     errors.add(:overlap, 'Horário sobrepõe com outra palestra!') unless overlapping_talks.empty?
+  end
+
+  def outside_event_bounds
+    not_in_event = event.start_date > start_date || event.end_date < end_date
+    errors.add(:out_of_bounds, 'Horário não está dentro do evento!') if not_in_event
   end
 end
