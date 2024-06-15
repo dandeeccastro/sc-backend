142618ae28cf55d4c772d4cce19d31e43ca112f7
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-15 13:32:54 -0300

    adding CPF to user and expecting it on login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,6 @@
 class AuthController < ApplicationController
   def login
-    @user = User.find_by(email: auth_params[:email])
+    @user = User.find_by(cpf: auth_params[:cpf])
     if @user&.authenticate(auth_params[:password])
       token = encode(uid: @user.id)
       time = Time.now + 24.hours.to_i
