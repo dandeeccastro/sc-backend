6dbfa1f792711a39ac0ed9c91bab22e15e449b2d
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-19 19:32:42 -0300

    null

--- db/migrate/20240619222346_add_type_and_category_to_talk.rb
+++ db/migrate/20240619222346_add_type_and_category_to_talk.rb
@@ -1,6 +1,6 @@
 class AddTypeAndCategoryToTalk < ActiveRecord::Migration[7.0]
   def change
-    add_reference :talks, :type, null: false, foreign_key: true
-    add_reference :talks, :category, null: false, foreign_key: true
+    add_reference :talks, :type, null: true, foreign_key: true
+    add_reference :talks, :category, null: true, foreign_key: true
   end
 end
