e5067140b79c937016e729432f436c4073064fcf
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-22 12:42:02 -0300

    no stringify needed

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,7 @@ class AuthController < ApplicationController
     if @user&.authenticate(auth_params[:password])
       token = encode(uid: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%d/%m/%Y %H:%M"), user: UserBlueprint.render(@user) }, status: :ok
+      render json: { token: token, exp: time.strftime("%d/%m/%Y %H:%M"), user: UserBlueprint.render_as_hash(@user) }, status: :ok
     else
       render json: { errors: 'CPF ou senha inválidos' }, status: :unauthorized
     end
