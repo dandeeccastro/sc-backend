b1f33af26badc14d4ffc8064fea3db0390d17459
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-23 14:40:01 -0300

    unauthenticated user can see talk

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,6 +1,6 @@
 class TalksController < ApplicationController
   before_action :set_talk, only: %i[show update destroy]
-  before_action :authenticate_user
+  before_action :authenticate_user, except: %i[show]
   before_action :authorized?, only: %i[create update destroy]
 
   def show
