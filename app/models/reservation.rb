e148650197c1dab367a10af92d82d486402bf77f
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-06-30 22:43:44 -0300

    changing reservation validation to Rails builtin

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,12 +1,6 @@
 class Reservation < ApplicationRecord
-  validate :unique_per_user
+  validates_uniqueness_of :merch_id, scope: [:user_id]
 
   belongs_to :user
   belongs_to :merch
-
-  def unique_per_user
-    conflicting_reservations = Reservation.where('user_id = :user_id AND merch_id = :merch_id AND id != :id',
-                                                 { user_id: user.id, merch_id: merch.id, id: id })
-    errors.add(:already_reserved, 'You already have a reservation for this merch!') unless conflicting_reservations.empty?
-  end
 end
