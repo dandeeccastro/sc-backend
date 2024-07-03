329e33c22914d93e9f93f922cbcfb672488f9fed
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-02 22:44:51 -0300

    typo

--- app/blueprints/notification_blueprint.rb
+++ app/blueprints/notification_blueprint.rb
@@ -1,4 +1,4 @@
 class NotificationBlueprint < Blueprinter::Base
-  fields :description, :created_date, :title
+  fields :description, :created_at, :title
   association :user, blueprint: UserBlueprint
 end
