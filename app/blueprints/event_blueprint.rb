6103fc88effbd5757dd7001e21c2fd81313fe69b
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-10 22:08:40 -0300

    passing event ID

--- app/blueprints/event_blueprint.rb
+++ app/blueprints/event_blueprint.rb
@@ -1,5 +1,6 @@
 class EventBlueprint < Blueprinter::Base
-  fields :id, :name, :slug, :start_date, :end_date, :registration_start_date
+  identifier :id
+  fields :name, :slug, :start_date, :end_date, :registration_start_date
 
   view :event do
     field :talks do |event| event.schedule end
