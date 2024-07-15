4d1f837efad5dc217ac6863050810bb1126c4714
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-15 00:20:15 -0300

    finalizing CRUD for types

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -1,6 +1,41 @@
 class TypeController < ApplicationController
+  before_action :authenticate_user
+  before_action :set_type, only: %i[update destroy]
+
   def index
     types = Type.all
     render json: TypeBlueprint.render(types), status: :ok
+  end
+
+  def create
+    @type = Type.new(type_params)
+    if @type.save
+      render json: TypeBlueprint.render(@type), status: :created
+    else
+      render json: { message: @type.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @type.update(type_params)
+      render json: TypeBlueprint.render(@type), status: :ok
+    else
+      render json: { message: @type.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @type.destroy
+    render json: { message: 'Tipo deletado com sucesso!' }, status: :ok
+  end
+
+  private
+
+  def set_type
+    @type = Type.find(params[:id])
+  end
+
+  def type_params
+    params.permit(:name, :color)
   end
 end
