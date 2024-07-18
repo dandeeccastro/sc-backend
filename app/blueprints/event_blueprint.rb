d3523db537481d865bf028e1ff86834c28576b65
Danilo Collares <daniloccastro@dcc.ufrj.br>
2024-07-17 22:31:20 -0300

    banner_url on event return

--- app/blueprints/event_blueprint.rb
+++ app/blueprints/event_blueprint.rb
@@ -1,6 +1,7 @@
 class EventBlueprint < Blueprinter::Base
   identifier :id
   fields :name, :slug, :start_date, :end_date, :registration_start_date
+  field :banner_url do |event| event.banner_url end
 
   view :event do
     field :talks do |event| event.schedule end
