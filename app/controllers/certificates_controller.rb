9a343f5478c5174b6700a20ca0af1d8676e6dbda
Danilo <daniloccastro@dcc.ufrj.br>
2024-07-24 18:25:39 -0300

    testing for controller requests

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -91,4 +91,9 @@ class CertificatesController < ActionController::Base
     criteria = @current_user.admin? || (@current_user.runs_event?(@event) && (@current_user.staff? || @current_user.staff_leader?))
     render json: { message: 'Unauthorized' } unless criteria
   end
+
+  def log_data
+    @event = Event.find(params[:event_id]) unless @event
+    AuditLogger.log(@event, "#{@current_user.name} #{@current_user.name} chamou #{params[:action]} na controller #{params[:controller]} (parâmetros: #{params.to_unsafe_h.inspect})")
+  end
 end
