cc7eddb2570c9407ebab7c1e32efd30196c4a668
Danilo <daniloccastro@dcc.ufrj.br>
2025-03-19 16:14:33 -0300

    adding proper domain and temporary admins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'venti-sandy.vercel.app'
+    origins 'localhost:8080', 'venti-sandy.vercel.app', 'venti.ic.ufrj.br'
 
     resource "*",
       headers: :any,
